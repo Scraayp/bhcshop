1aab262aa831b6ca01ab2ec00fa47cb976fb119f
Scraayp <scraayp@gmail.com>
2022-01-10 16:08:06 +0100

    Added videoland

---
+++ pages/videoland.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <iframe
+      src="https://videoland.nl"
+      title="Videoland"
+      width="1920"
+      height="1080"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
